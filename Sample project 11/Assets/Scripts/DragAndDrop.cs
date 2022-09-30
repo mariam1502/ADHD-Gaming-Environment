9aa2066f03f503b5425411946093068e56fdd3d9
mariam1502 <81592431+mariam1502@users.noreply.github.com>
2022-09-30 19:52:54 +0200

    mini puzzle game
    to prove the connection between python and unity

---
+++ Sample project 11/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    public GameObject SelectedPiece;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if(hit.transform.CompareTag("cat"))
+            {
+                if(!hit.transform.GetComponent<pieceScripe>().InRightPosition)
+                {
+                    SelectedPiece = hit.transform.gameObject;
+                    SelectedPiece.GetComponent<pieceScripe>().Selected = true;
+                }
+
+
+            }
+        }
+
+        if(SelectedPiece != null)
+        {
+            Vector3 MousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            SelectedPiece.transform.position = new Vector3(MousePoint.x,MousePoint.y,0);
+
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            SelectedPiece.GetComponent<pieceScripe>().Selected = false;
+            SelectedPiece = null;
+        }
+
+    }
+}
---
+++ Sample project 11/Assets/Scripts/pieceScripe.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class pieceScripe : MonoBehaviour
+{
+    private Vector3 RightPosition;
+    public bool InRightPosition;
+    public bool Selected;
+    void Start()
+    {
+        RightPosition = transform.position;
+        transform.position = new Vector3(Random.Range(2f,4f),Random.Range(6f,-2f));
+    }
+
+    void Update()
+    {
+     if(Vector3.Distance(transform.position,RightPosition)<0.5f)
+        {
+            if(!Selected)
+            {
+                transform.position = RightPosition;
+                InRightPosition = true;
+            }
+          
+        }
+    }
+}
