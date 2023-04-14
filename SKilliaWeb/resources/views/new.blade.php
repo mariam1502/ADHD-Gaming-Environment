a1e4c1e24fb53de4fa7a48fa6b2f2aa50bc173ca
ChristineAshraf <Christine1907011@miuegypt.edu.eg>
2023-04-14 02:04:37 +0200

    add new page

---
+++ SKilliaWeb/resources/views/new.blade.php
@@ -0,0 +1,19 @@
+@extends('layouts.master')
+
+@section('title')
+@endsection
+
+@section('css')
+{{-- add any css i want it will add to head.blade.php --}}
+@endsection
+
+@section('scripts')
+{{-- add any javascript code i want it will add to footer-scripts.blade.php --}}
+@endsection
+
+@section('page title') 
+@endsection
+
+@section('content')
+
+@endsection
